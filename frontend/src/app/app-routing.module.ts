729bfedc6e5628edeb8fdfb1f46e4dedb1f9fd08
mariorscode <mario.rodriguez11@alu.uclm.es>
2024-04-15 18:22:25 +0200

    acutalizado version de angular y componente calendario

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+
+const routes: Routes = [{ path: 'calendar', component: CalendarComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
